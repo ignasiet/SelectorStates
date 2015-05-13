7634b6023f4cfa12a8558287893316d948498f1f
Ignasi <ignasi.af@gmail.com>
2015-05-13 18:44:49 -0300

    Search Astar ready (need to test)

--- src/SearchNode.java
+++ src/SearchNode.java
@@ -12,6 +12,7 @@ public class SearchNode {
 	
 	public Hashtable<String, Integer> state;
 	public int heuristicValue;
+	public AbstractAction generatedBy = null;
 	
 	public SearchNode(Hashtable<String, Integer> state_copy){
 		state = new Hashtable<String, Integer>(state_copy);
@@ -19,6 +20,36 @@ public class SearchNode {
 	
 	public Hashtable<String, Integer> getState(){
 		return state;
+	}
+
+	public boolean contains(String predicate) {		
+		return state.containsKey(predicate);
+	}
+
+	public boolean canApply(AbstractAction a) {
+		for(String precondition : a._precond){
+			if(!state.containsKey(precondition)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public SearchNode applyAction(AbstractAction a) {
+		SearchNode node_sucessor = new SearchNode(state);
+		node_sucessor.generatedBy = a;
+		//1-Apply positive effects:
+		for(String effect_positive : a._Positive_effects){
+			node_sucessor.state.put(effect_positive, 1);
+		}
+		//2 - Apply negative effects:
+		for(String effect_negative : a._Negative_effects){
+			if(effect_negative.startsWith("~")){
+				effect_negative = effect_negative.substring(1);
+			}
+			node_sucessor.state.remove(effect_negative);
+		}
+		return node_sucessor;
 	}
 
 }
--- src/Searcher.java
+++ src/Searcher.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.PriorityQueue;
 import java.util.Comparator;
 
@@ -8,11 +11,14 @@ import java.util.Comparator;
 public class Searcher {
 	
 	private int nMax = 20;
-	PriorityQueue<SearchNode> pQueue;
+	private PriorityQueue<SearchNode> fringe;
+	private Domain domain_translated;
+	private Hashtable<SearchNode, Integer> _VisitedNodes = new Hashtable<SearchNode, Integer>();
+	private graphplanner gp;
 	
 	public Searcher(){
 		//init Priority queue
-		pQueue = new PriorityQueue<SearchNode>(nMax, 
+		fringe = new PriorityQueue<SearchNode>(nMax, 
 				new Comparator<SearchNode>(){
 					public int compare(SearchNode lhs, SearchNode rhs) {
 						return lhs.heuristicValue - rhs.heuristicValue;
@@ -22,15 +28,60 @@ public class Searcher {
 	
 	public void searchPlan(Domain domain){
 		//Add initial state with heuristic to the fringe
+		domain_translated = domain;
 		SearchNode initialState = new SearchNode(domain.state);
-		graphplanner gp = new graphplanner(initialState.getState(), domain.list_actions, domain.goalState);
+		gp = new graphplanner(initialState.getState(), domain.list_actions, domain.goalState);
 		if(!gp.fail){
 			System.out.println("Plano: " + gp.last_layer);
 			System.out.println("Valor heuristico: " + gp.heuristicValue());
 			for(int i=1;i<=gp.last_layer;i+=2){
 				System.out.println("Action level: " + gp._ActionPlan.get(i));
 			}
+			initialState.heuristicValue =  gp.heuristicValue();
+			fringe.add(initialState);
+			aStarSearch(fringe);
 		}
+	}
+	
+	public void aStarSearch(PriorityQueue<SearchNode> pQueue2){
+		while(!fringe.isEmpty()){
+			//node <- selectFrom(fringe)
+			SearchNode node = fringe.poll();
+			//If node is Goal, return Path
+			if(goalTest(node)){
+				returnPath(node);
+			}
+			//Else expand node
+			expand(node);
+		}
+	}
+	
+	private void expand(SearchNode node) {
+		Enumeration<String> e = domain_translated.list_actions.keys();
+		while(e.hasMoreElements()){
+			AbstractAction a = domain_translated.list_actions.get(e.nextElement().toString());
+			if(node.canApply(a)){
+				SearchNode node_sucessor = node.applyAction(a);				
+				gp = new graphplanner(node_sucessor.getState(), domain_translated.list_actions, domain_translated.goalState);
+				if(!gp.fail){
+					node_sucessor.heuristicValue = gp.heuristicValue();
+					fringe.add(node_sucessor);
+				}
+			}
+		}
+	}
+
+	private void returnPath(SearchNode node) {
+		System.out.println("GOAL!");
+	}
+
+	private boolean goalTest(SearchNode node){
+		for(String predicate : domain_translated.goalState){
+			if(!node.contains(predicate)){
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
