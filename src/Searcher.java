513257da2ab4825c38b7f6605c5c03b00e4d011c
Ignasi <ignasi.af@gmail.com>
2015-05-15 15:25:39 -0300

    Cleaning code

--- src/Searcher.java
+++ src/Searcher.java
@@ -18,12 +18,10 @@ public class Searcher {
 	private Hashtable<Hashtable<String, Integer>, Integer> _VisitedNodes = new Hashtable<Hashtable<String, Integer>, Integer>();
 	private Integer _GeneratedNodes = 0;
 	private Integer i = 0;
-	private ArrayList<Tupla> tupla_Solutions = new ArrayList<Searcher.Tupla>();
 	private ArrayList<ArrayList<SearchNode>> solution_path = new ArrayList<ArrayList<SearchNode>>();
 	private ArrayList<SearchNode> solution_nodes = new ArrayList<SearchNode>();
 	private ArrayList<SearchNode> nodes_toReplan = new ArrayList<SearchNode>();
 	private ArrayList<SearchNode> nodes_pending = new ArrayList<SearchNode>();
-	//private boolean nodes_pending = false;
 	private boolean success = false;
 	private Hashtable<String, Integer> observations_made = new Hashtable<String, Integer>();
 	
@@ -48,33 +46,22 @@ public class Searcher {
 		SearchNode initialState = new SearchNode(domain.state);
 		graphplanner gp = new graphplanner(initialState.getState(), domain.list_actions, domain.goalState);
 		if(!gp.fail){
-			/*System.out.println("Plano: " + gp.last_layer);
-			System.out.println("Valor heuristico: " + gp.heuristicValue());
-			for(int i=1;i<=gp.last_layer;i+=2){
-				System.out.println("Action level: " + gp._ActionPlan.get(i));
-			}*/
 			initialState.heuristicValue = gp.heuristicValue();
 			initialState.fCost = initialState.heuristicValue + initialState.pathCost;
 			Queue<SearchNode> fringe = initFringe();
 			initialState._ActionsApplied = new Hashtable<String, Integer>();
 			fringe.add(initialState);
 			searcherContingentPlan(fringe, domain);
-			//aStar(initialState);
-			//recursiveBFS(domain, initialState, Integer.MAX_VALUE);
-			//AndOrSearch(domain, initialState);
 		}
 	}
 	
 	public void searcherContingentPlan(Queue<SearchNode> fringe, Domain domain){
-		//SearchNode initialState = fringe.poll();
 		aStarSearch(fringe);
-		//recursiveBFS(domain, initialState, Integer.MAX_VALUE);
 		while(!nodes_toReplan.isEmpty()){
 			SearchNode node_r = nodes_toReplan.get(0);
 			nodes_toReplan.remove(0);
 			fringe.clear();
 			fringe.add(node_r);
-			//recursiveBFS(domain, fringe.poll(), Integer.MAX_VALUE);
 			aStarSearch(fringe);
 		}
 		System.out.println("Number of solutions found: " + solution_nodes.size());
@@ -124,74 +111,6 @@ public class Searcher {
 		}
 		if(fringe.isEmpty()){
 			System.out.println("SEARCH FAILED!");
-		}
-	}
-	
-	/** 
-	 * Retorna solução ou falha e um novo limite f-custo
-    */
-	public Tupla recursiveBFS(Domain domain, SearchNode node, Integer limit){
-		//Verify if is goal
-		if(goalTest(node)){
-			//System.out.println("This branch reached goal!");
-			returnPath(node);
-			Tupla return_t = new Tupla(node, 0);			
-			return return_t;
-		}
-		//Fringe init
-		Queue<SearchNode> fringe = initFringe();
-		Queue<SearchNode> sucessor = initFringe();
-		//Expand node
-		ArrayList<AbstractAction> applicable_action_list = matchApplicableActions(node);
-		for(AbstractAction action : applicable_action_list){
-			if(!(action.IsObservation && node._ActionsApplied.containsKey(action.Name))){
-				SearchNode node_created = expand(node, action);
-				if(!_VisitedNodes.containsKey(node_created.state)){
-					_GeneratedNodes++;
-					fringe.add(node_created);
-				}
-			}
-		}
-		if(fringe.isEmpty()){
-			System.out.println("This branch failed because there are no more applicable actions!");
-			return null;
-		}
-		for(SearchNode s : fringe){
-			s.fCost = max(s.fCost, node.fCost);
-			sucessor.add(s);
-		}
-		while(true){
-			//Order successors according to f
-			SearchNode best = sucessor.peek();
-			if(best.fCost > limit){
-				Tupla return_t = new Tupla(null, best.fCost);
-				//System.out.println("This branch failed!");
-				return return_t;
-			}
-			Integer alternativa;
-			if(sucessor.size()>1){
-				//Segundo melhor fCost...o atual?
-				//Eliminamos o primero da lista...e depois o colocamos de novo-> not good
-				PriorityQueue<SearchNode> copy_Queue = new PriorityQueue<SearchNode>(sucessor);
-				SearchNode first_best = copy_Queue.poll();
-				SearchNode alternative_best = copy_Queue.peek();
-				//sucessor.add(best);
-				alternativa = alternative_best.fCost;
-				//alternativa = sucessor.peek().fCost;
-			}
-			else{
-				alternativa = Integer.MAX_VALUE;
-			}
-			Tupla tupla_return = recursiveBFS(domain, best, min(limit, alternativa));
-			//return tupla_return;
-			if(tupla_return != null){
-				best.fCost = tupla_return.limit;
-				sucessor.poll();
-				sucessor.add(best);
-				if(tupla_return.node != null){
-					return tupla_return;
-				}
-			}
 		}
 	}
 	
@@ -276,32 +195,5 @@ public class Searcher {
 		n.fCost = n.pathCost + n.heuristicValue;
 		return n;
 	}
-	
-	private Integer max(Integer i1, Integer i2){
-		if(i1 >= i2){
-			return i1;
-		}else{
-			return i2;
-		}
-	}
-	
-	private Integer min(Integer i1, Integer i2){
-		if(i1 <= i2){
-			return i1;
-		}else{
-			return i2;
-		}
-	}
-	
-	class Tupla{
-		SearchNode node;
-		Integer limit;
-		
-		Tupla(SearchNode s, Integer a){
-	        this.node = s;
-	        this.limit = a;
-	    }
-	}
-	
 	
 }
